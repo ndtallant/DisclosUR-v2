8bacbe8982281f7eb985c4bd5574bfa9ef996f8b
Nick Tallant <ndtallant@uchicago.edu>
2018-12-04 23:12:52 -0600

    Permisison error for mount data

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,11 @@ FROM python:3.7
 LABEL maintainer "Nick Tallant <ndtallant@gmail.com>"
 
 # Flask app setup
-COPY . /app
-WORKDIR /app
+COPY . / 
+WORKDIR /
 RUN pip install -r requirements.txt 
 ENV FLASK_ENV=docker
 EXPOSE 5000
 
+CMD ["python", "flask_site/app.py"]
 # FROM postgres:10.6
--- flask_site/app.py
+++ flask_site/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask_debugtoolbar import DebugToolbarExtension
+#from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.debug = True
@@ -7,7 +7,7 @@ app.debug = True
 @app.route('/')
 def index():
     return render_template('index.html')
-
+'''
 @app.route('/results', methods=['POST'])
 def results():
     address = request.form['user_address'] 
@@ -16,3 +16,7 @@ def results():
 @app.route('/about')
 def about():
    return render_template('about.html')
+'''
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
